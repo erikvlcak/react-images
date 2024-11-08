c8a931f763ee28c14f7728273983280720a26dd7
erikvlcak <erikvlcak1@gmail.com>
2024-11-08 16:20:58 +0100

    merged

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-import { useState } from "react";
-=======
 import { useEffect, useState } from "react";
->>>>>>> main
 
 import SearchBar from "./SearchBar";
 import SearchResults from "./SearchResults";
@@ -11,13 +7,6 @@ import ImageDetails from "./ImageDetails.jsx";
 
 export default function Homepage() {
   const [images, setImages] = useState(null);
-<<<<<<< HEAD
-  const [keyword, setKeyword] = useState("");
-  const [detailImg, setDetailImg] = useState("");
-  const [showDetailImg, setShowDetailImg] = useState(false);
-
-  async function fetchImages() {
-=======
   const [keyword, setKeyword] = useState("nature");
   const [detailImg, setDetailImg] = useState("");
   const [showDetailImg, setShowDetailImg] = useState(false);
@@ -25,30 +14,10 @@ export default function Homepage() {
 
   async function fetchImages() {
     setLoading(true);
->>>>>>> main
     let url = `https://api.unsplash.com/search/photos?query=${keyword}&client_id=${apikey}&per_page=12`;
     let response = await fetch(url);
     let data = await response.json();
     setImages(data.results);
-<<<<<<< HEAD
-    console.log(keyword);
-    console.log(images);
-  }
-
-  return (
-    <>
-      <SearchBar setKeyword={setKeyword} fetchImages={fetchImages} />
-      <SearchResults
-        images={images}
-        keyword={keyword}
-        setDetailImg={setDetailImg}
-        showDetailImg={showDetailImg}
-        setShowDetailImg={setShowDetailImg}
-      />
-      {showDetailImg ?? (
-        <ImageDetails detailImg={detailImg} showDetailImg={showDetailImg} setShowDetailImg={setShowDetailImg} />
-      )}
-=======
     console.log(images);
     setLoading(false);
   }
@@ -75,7 +44,6 @@ export default function Homepage() {
           setShowDetailImg={setShowDetailImg}
         />
       )}
->>>>>>> main
     </>
   );
 }
