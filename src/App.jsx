034478d7ea398e991393b82f93a5f64fde618ffe
Aidana <kayelike8@gmail.com>
2024-11-08 11:58:24 +0100

    merge main solved conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Homepage from "./Homepage";
+import AuthorDetail from "./AuthorDetail";
 import "./App.css";
 
 export default function App() {
   return (
     <>
       <Homepage />
+      <AuthorDetail />
     </>
   );
 }
