de22b927213176e77ade5a65629a5bc0f981c1cf
Aidana <kayelike8@gmail.com>
2024-11-08 16:06:08 +0100

    aidana final

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
-import "./App.css";
+<<<<<<< Updated upstream
+=======
+import Homepage from "./Homepage";
 import AuthorDetail from "./AuthorDetail";
+import { Route, Routes } from "react-router-dom";
+>>>>>>> Stashed changes
+import "./App.css";
+import "./Portfolio.css"
+
 
 export default function App() {
+<<<<<<< Updated upstream
   return <div>
     <AuthorDetail/>
   </div>;
+=======
+
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/author/:username" element={<AuthorDetail />} />
+      </Routes>
+    </>
+  );
+>>>>>>> Stashed changes
 }
--- src/AuthorDetail.jsx
+++ src/AuthorDetail.jsx
@@ -1,82 +1,82 @@
 import { useEffect, useState } from "react";
 import apikey from "./../keys.js";
+import { useParams } from "react-router-dom";
 
 export default function AuthorDetail() {
-  const [portfolio, setPortfolio] = useState(null);
-  const [images, setImages] = useState(null);
+    const [images, setImages] = useState(null);
+    const [user, setUser] = useState(null);
+    const { username } = useParams();  
 
-  const getAuthorPortfo = async () => {
-    const response = await fetch(
-      `https://api.unsplash.com/search/users?page=1&query=nas&client_id=${apikey}&per_page=1`
-    );
-    const data = await response.json();
-    console.log(data.results);
-    
-    setPortfolio(data.results);
-  };
+    const getAuthorPortfo = async () => {
+        const response = await fetch(`https://api.unsplash.com/users/${username}?client_id=${apikey}`);
+        const data = await response.json();
+        console.log(data);
+        setUser(data);
+    }
 
-  const getImages = async () => {
-    const response = await fetch(
-      `https://api.unsplash.com/users/${portfolio && portfolio[0]?.username}/photos?client_id=${apikey}`
-    );
-    const data = await response.json();
-    console.log(data);
-    setImages(data); 
-  };
+    const getImages = async () => {
+        const response = await fetch(
+            `https://api.unsplash.com/users/${
+            user && user.username
+            }/photos?client_id=${apikey}`
+        );
 
-  useEffect(() => {
-    getAuthorPortfo();
-  }, []);
+        const data = await response.json();
+        setImages(data);
+    }
 
-  useEffect(() =>{
-    getImages();
-  },[portfolio])
+    useEffect(() => {
+        getAuthorPortfo();
+    }, [username]);
 
-  return (
-    <>
-      {portfolio ? (
-        <>
-          {portfolio.map((user) => {
-            return (
-              <div className="portoflio-container" key={user.id}>
-                <div className="portfolio">
-                  <h2>
-                    Welcome to {user.first_name} {user.last_name}'s Portfolio
-                  </h2>
-                  <div className="user_info">
-                    <p>
-                      <strong>Author name:</strong> {user.name}
-                    </p>
-                    <p>
-                      <strong>Username:</strong> {user.username}
-                    </p>
-                    <img src={user.profile_image.large} alt="" />
-                  </div>
-                  <a href={user.links.followers}>Followers</a> <br />
-                  <a href={user.links.photos}>More photos to inspiration</a>
+    useEffect(() => {
+        getImages();
+    }, [user]);
+
+    return (
+      <>
+        {user ? (
+          <>
+            <div className="portfolio" key={user.id}>
+              <h2>Welcome to {user.name}'s Portfolio</h2>
+              <div className="user_info">
+                <img src={user.profile_image.large} alt="" />
+                <div className="portfolio_name">
+                  <p>
+                    <strong>Author name:</strong> {user.name}
+                  </p>
+                  <p>
+                    <strong>Username:</strong> {user.username}
+                  </p>
+                  <p>
+                    <strong>Bio:</strong> {user.bio}
+                  </p>
+                  <p>
+                    <strong>Downloads:</strong> {user.downloads}
+                  </p>
                   <p>
                     <strong>Total likes: </strong>
                     {user.total_likes}
                   </p>
                 </div>
-
-                <h3> {user.username}'s works</h3>
-                <div className="portfolio_photos">
-                  {images?.map((image) => {
-                    return (
-                      <div key={image.id}>
-                        <img src={image.urls.small} alt="" />
-                      </div>
-                    );
-                  })}
-                </div>
               </div>
-            );
-          })}
-        </>
-      ) : (
-        <p>Loading author details...</p>
-      )}
-    </>
-  );
+            </div>
+
+            <h3> {user.name}'s works</h3>
+            <div className="portfolio_photos">
+              {images.map((image) => {
+                return (
+                  <div key={image.id}>
+                    <img src={image.urls.small} alt="" />
+                  </div>
+                );
+              })}
+            </div>
+          </>
+        ) : (
+          <p>Loading...</p>
+        )}
+      </>
+    );
+
 }
